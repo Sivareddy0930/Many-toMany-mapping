c4136e544de4435e2ed446882975b0ffe14543c9
Efund <Sivareddy0930>
2024-11-15 15:00:47 +0530

    tasks

--- src/main/java/com/dev/manytomany/entity/Project.java
+++ src/main/java/com/dev/manytomany/entity/Project.java
@@ -1,30 +1,30 @@
 package com.dev.manytomany.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Setter
-@Getter
+@Data
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String description;
 
 //    @ManyToMany(mappedBy = "projects", cascade =  CascadeType.ALL,fetch = FetchType.LAZY)
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinTable(name = "EMPLOYEE_PROJECT_TABLE",
+    @JoinTable(name = "EMPLOYEE_PROJECT_TABLE" ,
             joinColumns ={
                     @JoinColumn(name = "project_id", referencedColumnName = "id")
             } ,
@@ -35,19 +35,4 @@ public class Project {
     private Set<Employee> employees;
 
 
-    public void addEmployee(Employee employee) {
-        if (!employees.contains(employee)) {  // Check if the employee is already associated with this project
-            employees.add(employee);
-            if (employee.getProjects() == null) {
-                employee.setProjects(new HashSet<>());
-            }
-            employee.getProjects().add(this);  // Add the current project to the employee's projects
-        }
-    }
-
-
-    public void removeEmployee(Employee employee) {
-        employees.remove(employee);
-        employee.getProjects().remove(this);
-    }
 }
--- src/main/java/com/dev/manytomany/service/EmployeeProjectService.java
+++ src/main/java/com/dev/manytomany/service/EmployeeProjectService.java
@@ -1,19 +1,27 @@
 package com.dev.manytomany.service;
 
+import com.dev.manytomany.dto.AddEmployeeToProjectDto;
+import com.dev.manytomany.dto.AddProjectToEmployeeDto;
+import com.dev.manytomany.dto.EmployeeDto;
 import com.dev.manytomany.entity.Employee;
 import com.dev.manytomany.entity.Project;
 import com.dev.manytomany.repository.EmployeeRepository;
 import com.dev.manytomany.repository.ProjectRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.Set;
+
+import java.util.*;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.HashSet;
+
 import java.util.Set;
 
+@Slf4j
 @Service
 public class EmployeeProjectService {
 
@@ -23,26 +31,12 @@ public class EmployeeProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Transactional
     public String saveProjectWithEmployees(Project project) {
-//        Project project = new Project();
-//        project(Project.getProjectCode());
-//        project.setProjectName(Project.getProjectName());
-//        project.setTechnologyUsed(Project.getTechnologyUsed());
-//
-//        Set<Employee> employees = new HashSet<>();
-//        for (EmployeeDTO employeeDTO : Project.getEmployees()) {
-//            Employee employee = new Employee();
-//            employee.setEmployeeId(employeeDTO.getEmployeeId());
-//            employee.setName(employeeDTO.getName());
-//            employee.setEmail(employeeDTO.getEmail());
-//            employee.setTechnicalSkill(employeeDTO.getTechnicalSkill());
-//
-//            project.addEmployee(employee);
-//            employees.add(employee);
-//        }
-
-         projectRepository.save(project);
+        projectRepository.save(project);
         return "Project created successfully";
     }
 
@@ -56,17 +50,52 @@ public class EmployeeProjectService {
     }
 
     @Transactional
-    public String addEmployeeToProjectBasedOnId(Long projectId, Employee employee) {
+    public String addEmployeeToProjectBasedOnId(AddEmployeeToProjectDto addEmployeeToProjectDto) {
 
-        Project project = projectRepository.findById(projectId)
-                .orElseThrow(() -> new RuntimeException("Project not found with ID: " + projectId));
+        Project projectById = projectRepository.findById(addEmployeeToProjectDto.getProjectId())
+                .orElseThrow(() -> new RuntimeException("Project not found with ID: " + addEmployeeToProjectDto.getProjectId()));
 
-//        Add the employee to the project and we here we have to link it bidirectionally.
-        project.addEmployee(employee);
+        EmployeeDto employeeDto = addEmployeeToProjectDto.getEmployeeDto();
 
-        employeeRepository.save(employee);
+        Employee emp1 = new Employee(employeeDto.getName(),employeeDto.getEmail(),employeeDto.getTechnicalSkills());
+
+        Employee savedEmployee = employeeRepository.save(emp1);
+
+        String insertQuery = "INSERT INTO EMPLOYEE_PROJECT_TABLE (project_id, employee_id) VALUES (?, ?)";
+
+        Query query = entityManager.createNativeQuery(insertQuery);
+        query.setParameter(1, addEmployeeToProjectDto.getProjectId());
+        query.setParameter(2, savedEmployee.getEid());
+
+        query.executeUpdate();
+
 
         return "Employee added to project successfully";
     }
+
+    @Transactional
+    public String addProjectToEmployeeBasedOnId(AddProjectToEmployeeDto addProjectToEmployeeDto) {
+
+
+        Optional<Employee> byId = employeeRepository.findById(addProjectToEmployeeDto.getEmployeeId());
+        log.info("ById: " + addProjectToEmployeeDto.getEmployeeId());
+        if (!byId.isPresent()) {
+            throw new RuntimeException("Employee not found with ID: " + addProjectToEmployeeDto.getEmployeeId());
+        }
+
+
+        Project project = new Project(addProjectToEmployeeDto.getProjectDto().getName(), addProjectToEmployeeDto.getProjectDto().getDescription());
+        Project savedProject = projectRepository.save(project);
+        String insertQuery = "INSERT INTO EMPLOYEE_PROJECT_TABLE (project_id, employee_id) VALUES (?, ?)";
+
+        Query query = entityManager.createNativeQuery(insertQuery);
+        query.setParameter(1, savedProject.getId());
+        query.setParameter(2, addProjectToEmployeeDto.getEmployeeId());
+
+        query.executeUpdate();
+        return "Project added to Employee successfully";
+
+    }
 }
+
 
