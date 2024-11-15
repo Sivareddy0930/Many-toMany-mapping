846cf2422c74edc4a19b55744c81eff2a7b3c533
Efund <Sivareddy0930>
2024-11-15 23:24:43 +0530

    task Many to Many

--- src/main/java/com/dev/manytomany/repository/ProjectRepository.java
+++ src/main/java/com/dev/manytomany/repository/ProjectRepository.java
@@ -2,6 +2,16 @@ package com.dev.manytomany.repository;
 
 import com.dev.manytomany.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ProjectRepository extends JpaRepository<Project, Long> {}
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Project p WHERE p.id = :id")
+    void deleteProjectById(@Param("id") Long projectId);
+}
 
